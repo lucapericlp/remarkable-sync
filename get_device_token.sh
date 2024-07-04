61a1c336a5b07261175b00aad42e6cf7e9bfa860
Luca Peric <lucaperic.lp@gmail.com>
2024-07-04 12:16:43 +0100

    Updating get_device_token.sh

--- get_device_token.sh
+++ get_device_token.sh
@@ -1,4 +1,4 @@
-for log in "$(find "/Users/lucaperic/Library/Application Support/Google/Chrome/Default/Local Extension Settings/bfhkfdnddlhfippjbflipboognpdpoeh" -iname '*.log')"; do
+for log in "$(find "$HOME/Library/Application Support/Google/Chrome/Default/Local Extension Settings/bfhkfdnddlhfippjbflipboognpdpoeh" -iname '*.log')"; do
   strings "${log}" | grep -1 deviceToken | cut -d '"' -f2
   # strings "${log}"
 done
